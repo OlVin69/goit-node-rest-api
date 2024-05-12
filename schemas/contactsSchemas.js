9b807b8da36cf934355287c2dec826ab7c8bb1ac
OlVin69 <aviniichuk@gmail.com>
2024-05-12 17:47:17 +0300

    add models

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,10 +4,17 @@ export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
+    favorite: Joi.boolean().required(),
 });
 
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
     email: Joi.string(),
     phone: Joi.string(),
-})
+    favorite: Joi.boolean(),
+});
+
+export const updateStatusContactSchema = Joi.object({
+    
+    favorite: Joi.boolean().required(),
+});
